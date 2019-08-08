eb602d145de485143784a1e94f8013b433a9540a
Jakgrit Chuenboon <jakgritchuenboon@jakgrits-MacBook-Pro.local>
2019-08-08 11:19:56 +0700

    random function

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,19 +3,27 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         var rollButton: Button = findViewById(R.id.roll_button)
+        var resultText: TextView = findViewById(R.id.result_text)
 
         rollButton.setOnClickListener { rollDice() }
+        resultText.text = "Dice Rolled!"
     }
 
     private fun rollDice(){
+        var resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = Random.nextInt(6) + 1
+        resultText.text = randomInt.toString()
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
     }
 }
