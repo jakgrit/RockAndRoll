9c777b6c3e11abda79ecd55bac3d3cfcb1161e64
Jakgrit Chuenboon <jakgritchuenboon@jakgrits-MacBook-Pro.local>
2019-08-08 15:52:47 +0700

    ImageView added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -14,16 +15,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var rollButton: Button = findViewById(R.id.roll_button)
-        var resultText: TextView = findViewById(R.id.result_text)
 
         rollButton.setOnClickListener { rollDice() }
-        resultText.text = "Dice Rolled!"
     }
 
     private fun rollDice(){
-        var resultText: TextView = findViewById(R.id.result_text)
         val randomInt = Random.nextInt(6) + 1
-        resultText.text = randomInt.toString()
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+//        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
     }
 }
